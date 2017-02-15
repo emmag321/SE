8de4a60fe10b869a100045b1bc483bd2955fd5a1
emmag321 <emmag321@gmail.com>
2017-02-15 20:52:28 +0000

    Add files via upload

---
+++ Software Prototype/software/CancelBooking.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace software
+{
+    public partial class CancelBooking : Form
+    {
+
+        public CancelBooking()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Software Prototype/software/UpdateCar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace software
+{
+    public partial class UpdateCar : Form
+    {
+        public UpdateCar()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
